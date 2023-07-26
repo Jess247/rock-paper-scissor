eeb09dbe2e1393f60c72db5c9be1cab374bf23e8
Jess247 <jesss8692@hotmail.com>
2023-07-26 20:03:01 +0200

    Update game function
    Loop game 5 times and log result
    
    Track score

--- logic.js
+++ logic.js
@@ -1,19 +1,3 @@
-/*
-generate random computer choice
-prompt user to enter a choice
-compare user choice and computer choice
-    if user choice and computer are equal
-        log it's a draw
-    if user choice is rock and comp choice is scissor
-       log user wins
-    if user choice is paper and comp choice is rock
-       log user wins
-    if user choice is scissor and comp choice is paper
-       log user wins
-    else 
-        log comp wins
-*/
-
 // generate random computer choice
 function getComputerChoice() {
     let randNum = Math.floor(Math.random() * 3) + 1;
@@ -34,12 +18,6 @@ function getComputerChoice() {
     return choice;
 }
 
-
-// prompt user to enter a choice
-const userChoice = prompt("Choose Rock, Paper or Scissor: ").toLowerCase();
-const compChoice = getComputerChoice();
-console.log(`${userChoice} ${compChoice}`);
-
 // play round
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === "rock" && computerSelection === "scissor" ||
@@ -47,10 +25,41 @@ function playRound(playerSelection, computerSelection) {
         playerSelection === "scissor" && computerSelection === "paper") {
         return `Win! ${playerSelection} beats ${computerSelection}`;
     } else if (playerSelection === computerSelection) {
-        return "It's a tie";
+        return `It's a tie! ${computerSelection} & ${playerSelection}`;
     }else {
         return `Lose! ${computerSelection} beats ${playerSelection}`;
     }
 }
 
-console.log(playRound(userChoice, compChoice));
+// console.log(playRound(userChoice, compChoice));
+
+function game() {
+    let counter = 0;
+    let currentResult;
+    let userScore = 0;
+    let compScore = 0;
+
+    while(counter < 5) {
+        const userChoice = prompt("Choose Rock, Paper or Scissor: ").toLowerCase();
+        const compChoice = getComputerChoice();
+        
+        currentResult = playRound(userChoice, compChoice);
+        console.log(currentResult);
+        if (currentResult.includes("Win")) {
+            userScore++;
+        } else if (currentResult.includes("Lose")){
+            compScore++;
+        }
+        ++counter;
+    }
+    if (userScore > compScore) {
+        console.log("You win!");
+    } else if (userScore === compScore) {
+        console.log("It's a tie!");
+    } else {
+        console.log("You lose!");
+    }
+
+}
+
+game();

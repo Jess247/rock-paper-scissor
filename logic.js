7560a48b6c49eea5df52ef085f9a4d1e3ad7fe3a
Jess247 <jesss8692@hotmail.com>
2023-08-06 20:19:34 +0200

    Update play function

--- logic.js
+++ logic.js
@@ -4,6 +4,9 @@ const resultMsg = document.querySelector('.result-msg');
 const score = document.querySelector('.score');
 resultMsg.textContent = "Let see who'll win!";
 
+let userScore = 0;
+let compScore = 0;
+
 btns.forEach(btn => {
     btn.addEventListener('click', playRound);
 });
@@ -36,40 +39,29 @@ function playRound(e) {
         userChoice === "paper" && compChoice === "rock" ||
         userChoice === "scissors" && compChoice === "paper") {
         resultMsg.textContent = `Win! ${userChoice} beats ${compChoice}`;
+        userScore++;
+        console.log(userScore);
     } else if (userChoice === compChoice) {
         resultMsg.textContent = `It's a tie! ${compChoice} & ${userChoice}`;
     }else {
         resultMsg.textContent = `Lose! ${compChoice} beats ${userChoice}`;
+        compScore++;
+        console.log(compScore);
     }
 }
 
 // play game
 function game() {
-    let counter = 0;
-    let currentResult;
-    let userScore = 0;
-    let compScore = 0;
-
-    // play game rounds
-    // get round result and output
-    currentResult = playRound(this.id);
-    console.log(currentResult);
-    // increse score
-    if (currentResult.includes("Win")) {
-        userScore++;
-        score.textContent = userScore;
-    } else if (currentResult.includes("Lose")){
-        compScore++;
-    }
-
 
     // log result
-    if (userScore > compScore) {
-        console.log("You win!");
-    } else if (userScore === compScore) {
-        console.log("It's a tie!");
-    } else {
-        console.log("You lose!");
+    if (userScore + compScore === 5) {
+        if (userScore > compScore) {
+            console.log("You win!");
+        } else if (userScore === compScore) {
+            console.log("It's a tie!");
+        } else {
+            console.log("You lose!");
+        }
     }
 
 }
